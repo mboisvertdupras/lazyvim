b1c47ed4969b980d772380a55d997a5ca16a8c48
Marc Boisvert <marcboisvert.dupras@outlook.com>
2024-09-17 08:02:18 -0400

    feat(.gitignore): add .aider* to ignored files
    feat(keymaps): add keymap to close all buffers except current one
    
    feat(telescope): add fzf-native extension to telescope for improved performance

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Remove Save on Ctrl-s
 vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
+
+-- Close all buffers except the current one
+vim.keymap.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>")
